f5f0ac82a1378b17afbb55596dbe873543d322e4
Den Abad <dennis.abad6@gmail.com>
2022-11-03 17:34:34 +1100

    Changed GameManager to Singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,20 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private List<string> words;
+    public static GameManager Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
 
     void Start()
     {
